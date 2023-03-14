ee3816f1fb6b0c1d7a08870f351f0d138f488ba7
khang <dokhang295@gmail.com>
2023-03-14 20:16:59 +0700

    update docker host

--- scripts/pull_image.sh
+++ scripts/pull_image.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 294962230460.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 294962230460.dkr.ecr.us-east-1.amazonaws.com
 docker pull 294962230460.dkr.ecr.us-east-1.amazonaws.com/app:latest
