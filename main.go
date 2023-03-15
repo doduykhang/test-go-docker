4e9242655e845d884e972a904eba478f34a8a6dc
khang <dokhang295@gmail.com>
2023-03-15 23:03:36 +0700

    test-pipeline

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	      r := chi.NewRouter()
 	      r.Use(middleware.Logger)
 	      r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		            w.Write([]byte("welcome 3"))
+		            w.Write([]byte("welcome 4"))
 	      })
         fmt.Println("Server running at port 3000")
 	      http.ListenAndServe(":3000", r)
