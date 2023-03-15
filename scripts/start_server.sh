c99b0c681e8ec7a5c08323a5782dc27458a89fc4
khang <dokhang295@gmail.com>
2023-03-15 11:04:37 +0700

    docker run detatch

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -p 3000:3000 --name app 294962230460.dkr.ecr.us-east-1.amazonaws.com/app:latest
+docker run -p 3000:3000 --name app -d 294962230460.dkr.ecr.us-east-1.amazonaws.com/app:latest
